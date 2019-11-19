0543067c48743c1cbc7c40ec7ca6c0c767cfca0b
Bijaya <mohanty.bijaya87@gmail.com>
2019-11-19 18:40:18 +0530

    controller ------------ company: add company name directly without company type id. JobPosting: report of  last inserted record view ----- modify form and script
    added report on jobposting module
    added active and deactive on job posting module
    added edit option on jobpostive module.
    font changed karla to roboto condence
    
    210919:
    -------
    applicant requirement process start
    
    270919
    -------
    recruitment module completed
    report part is pending
    
    151019
    -------
    details view added on requirement
    
    191119
    -------
    resouces module added
    1.resource type
    2. assurance type //warranty/guaranty etc
    3.assurance
    4.company
    5.period type //hour /months /year etc

--- application/views/dashboard/dashBoardReports.php
+++ application/views/dashboard/dashBoardReports.php
@@ -4,19 +4,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div class="row">
     <div class="col-sm-12">
-      <div class="shadowColor">Employee Details</div>
+      <div class="shadowColor">Employee Details <span class="pull-right"><i class="fa fa-users" style="color: gold"></i>&nbsp;23</span></div>
         <br>
-        <div class="shadowColor">Recruitment Details</div>
+        <div class="shadowColor">Recruitment Details <span class="pull-right"><i class="fa fa-user-plus" style="color: gold"></i>&nbsp;13</span></div>
         <br>
-        <div class="shadowColor">Job Posting Details</div>
+        <div class="shadowColor">Job Posting Details <span class="pull-right"><i class="fa fa-file-powerpoint" style="color: gold"></i>&nbsp;29</span></div>
         <br>
-        <div class="shadowColor">Resource Details</div>
+        <div class="shadowColor">Resource Details <span class="pull-right"><i class="fa fa-suitcase" style="color: gold"></i>&nbsp;43</span></div>
         <br>
-        <div class="shadowColor">Salary Details</div>
+        <div class="shadowColor">Salary Details <span class="pull-right"><i class="fa fa-rupee" style="color: gold"></i>&nbsp;123</span></div>
         <br>
-        <div class="shadowColor">Expenditure Details</div>
+        <div class="shadowColor">Expenditure Details <span class="pull-right"><i class="fa fa-money-bill-alt" style="color: gold"></i>&nbsp;113</span></div>
         <br>
-        <div class="shadowColor">Task Management</div>
+        <div class="shadowColor">Task Management <span class="pull-right"><i class="fa fa-tasks" style="color: gold"></i>&nbsp;14</span></div>
         <br>
     </div>
 </div>
