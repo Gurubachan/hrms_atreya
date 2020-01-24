af21158a4d5303fb2306ed73ebd95a2c25565da9
Bijaya <mohanty.bijaya87@gmail.com>
2020-01-24 10:11:26 +0530

    Update Employee2.php

--- application/controllers/Employee2.php
+++ application/controllers/Employee2.php
@@ -4,6 +4,7 @@ date_default_timezone_set('Asia/Kolkata');
 class Employee extends CI_Controller {
     public function __construct()
     {
+        parent::__construct();
     }
     public function create_employee_type(){
         try{
